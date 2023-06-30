bb6c54e1a6d076b507cb47d9b0f26bef0ad519f8
James Costa <jamescosta4747@gmail.com>
2023-06-29 21:52:46 -0300

    Node API

---
+++ javascript/oo/oo3.js
@@ -0,0 +1,17 @@
+// let pessoa2 = new Object(nome,idade,altura,peso)
+// pessoa2.nome = 'Ana'
+// pessoa2.nome = '18'
+// pessoa2.nome = '155'
+// pessoa2.nome = '40'
+
+// const pessoa = {
+//     nome: 'João',
+//     sobrenome: 'Silva',
+    
+//     get nomeCompleto() {
+//       return this.nome + ' ' + this.sobrenome;
+//     }
+//   };
+  
+//   console.log(pessoa.nomeCompleto); // Saída: João Silva
+  
